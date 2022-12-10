ebedcacb134b1386da11e7d34fb86151af02e2d8
Juliette <97114875+Juliette02@users.noreply.github.com>
2022-12-10 18:48:33 +0100

    for prod

--- BlueVillageProd/src/Controller/DashboardController.php
+++ BlueVillageProd/src/Controller/DashboardController.php
@@ -13,43 +13,43 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DashboardController extends AbstractController
 {
-    #[Route('/caperyear', name: 'caperyear')]
+    #[Route('/caperyear', name: 'caperyear')] //-- Chiffre d'affaires mois par mois pour une année sélectionnée
     public function CAPerYear(DetailCommandeRepository $repo)
     {
         return new JsonResponse($repo->CAPerYear());
     }
     
-    #[Route('/caperfou', name: 'caperfou')]
+    #[Route('/caperfou', name: 'caperfou')] //Chiffre d'affaires généré pour un fournisseur
     public function CAPerFou(DetailCommandeRepository $repo)
     {
         return new JsonResponse($repo->CAPerFou());
     }
 
-    #[Route('/top10commande', name: 'top10commande')]
+    #[Route('/top10commande', name: 'top10commande')] //TOP 10 des produits les plus commandés pour une année sélectionnée (référence et nom du produit, quantité commandée, fournisseur)
     public function TOP10Commande(DetailCommandeRepository $repo)
     {
         return new JsonResponse($repo->TOP10Commande());
     }
 
-    #[Route('/top10remu', name: 'top10remu')]
+    #[Route('/top10remu', name: 'top10remu')] // TOP 10 des produits les plus rémunérateurs pour une année sélectionnée (référence et nom du produit, marge, fournisseur)
     public function TOP10Remu(DetailCommandeRepository $repo)
     {
         return new JsonResponse($repo->Top10Remu());
     }
 
-    #[Route('/top10clients', name: 'top10clients')]
+    #[Route('/top10clients', name: 'top10clients')] //Top 10 des clients en nombre de commandes ou chiffre d'affaires
     public function TOP10Clients(DetailCommandeRepository $repo)
     {
         return new JsonResponse($repo->Top10Clients());
     }
 
-    #[Route('/caperclients', name: 'caperclients')]
+    #[Route('/caperclients', name: 'caperclients')] //Répartition du chiffre d'affaires par type de client
     public function CAPerClients(DetailCommandeRepository $repo)
     {
         return new JsonResponse($repo->CAParClient());
     }
 
-    #[Route('/nbcommandelivraison', name: 'nbcommandelivraison')]
+    #[Route('/nbcommandelivraison', name: 'nbcommandelivraison')] //Nombre de commandes en cours de livraison.
     public function NbCommandesLivraison(LivraisonRepository $repo)
     {
         return new JsonResponse($repo->NbCommandesLivraison());
